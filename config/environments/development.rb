993e2ca667e23f61eea9999701488beaeb83267d
Afton Downey <downeydillon@gmail.com>
2016-05-14 10:25:41 -0700

    Configure letter opener mail delivery for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
   # config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 
-
+  config.action_mailer.delivery_method = :letter_opener
 
 
 
